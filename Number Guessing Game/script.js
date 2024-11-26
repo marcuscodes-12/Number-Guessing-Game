674c0fb614f82cc51d4cebd90fcf5cca7287427d
marcuscodes-12 <marcuscodes_@hotmail.com>
2024-11-26 18:40:23 +0530

    Add files via upload

---
+++ Number Guessing Game/script.js
@@ -0,0 +1,65 @@
+const randomNum = Math.floor(Math.random() * 50) + 1;
+// console.log = randomNum;
+let guessCount = 6;
+
+function verifyNumber() {
+  guessCount--;
+  let userInput = document.getElementById("userInput").value;
+  let suggestion = document.getElementById("suggestion");
+  let result = document.getElementById("result");
+  let guessCountalert = document.getElementById("guessCountalert");
+
+  //   alert(randomNum);
+  console.log(userInput);
+
+  let apUserInput = parseInt(userInput);
+
+  if (apUserInput === randomNum) {
+    result.textContent = "you winn!!";
+    result.style.color = "green";
+    // suggestion.textContent = " ";
+    // suggestion.style.color = "white";
+    guessCountalert.textContent = " ";
+    guessCountalert.style.color = "white";
+
+    suggestion.textContent = "Reloading in 3 seconds...";
+    suggestion.style.color = "green";
+    guessCountalert.textContent = " ";
+    setTimeout(() => {
+      location.reload();
+    }, 3000);
+    // reset();
+  } else {
+    if (apUserInput < randomNum) {
+      suggestion.textContent = "Guess Higher";
+      suggestion.style.color = "black";
+    } else {
+      suggestion.textContent = "Guess Lower";
+      suggestion.style.color = "black";
+    }
+    if (guessCount >= 0) {
+      guessCountalert.textContent =
+        "Only " + guessCount + " chances remaining.";
+      guessCountalert.style.color = "red";
+    }
+
+    if (guessCount === 0) {
+      result.textContent = "you lost";
+      result.style.color = "red";
+      document.getElementById("userInput").disabled = true;
+
+      suggestion.textContent = "Reloading in 3 seconds...";
+      suggestion.style.color = "red";
+      guessCountalert.textContent = " ";
+      setTimeout(() => {
+        location.reload();
+      }, 3000);
+
+      // reset();
+    }
+  }
+}
+
+function reset() {
+  location.reload();
+}
